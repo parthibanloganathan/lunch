676c4a5449cce51ab8b2219c39e53dc598780466
Parthiban Loganathan <pl2487@columbia.edu>
2015-06-08 21:46:26 -0700

    Make lib

---
+++ email_lib.py
@@ -0,0 +1,26 @@
+import yagmail
+import datetime
+import json
+import random
+
+def send_email(recipients, cafes):
+    ''' Send an email to recipients '''
+    with open('config.json') as config:
+        data = json.load(config)['email']
+
+    recipient_addresses, names = [address for (address, _) in recipients], [name for (_, name) in recipients]
+
+    sender = str(data['sender'])
+    yag = yagmail.Connect(sender)
+
+    people = ', '.join(names[:-1])
+    last_person = names[-1]
+    cafe = random.choice(cafes)
+    signup_link = data['signup_link']
+
+    date = datetime.datetime.now()
+    subject = str(data['subject']) + str(date.month) + '/' + str(date.day)
+    message = str(data['message'])
+    contents = [message.format(people, last_person, cafe, signup_link)]
+
+    yag.send(recipient_addresses, subject, contents) 
